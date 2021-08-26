869ed3ab99a4049a8e6f2779718e9ff61f5b4aed
Your Nameclear <Cybernetortechnologies@gmail.com>
2021-08-26 01:34:24 +0100

    First commit(2)

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
     region = "eu-west-1"
-    profile = "terraform_project"
+    # profile = "terraform_project"
 }
 
 
@@ -12,73 +12,73 @@ resource "aws_vpc" "terraform_ec2_vpc" {
 }
 
 
-# # Then Create a subnet
-# resource "aws_subnet" "terraform_ec2_subnet" {
-#   vpc_id            = aws_vpc.terraform_ec2_vpc.id
-#   cidr_block        = aws_vpc.terraform_ec2_vpc.cidr_block
-#   availability_zone = "eu-west-1a"
+# Then Create a subnet
+resource "aws_subnet" "terraform_ec2_subnet" {
+  vpc_id            = aws_vpc.terraform_ec2_vpc.id
+  cidr_block        = aws_vpc.terraform_ec2_vpc.cidr_block
+  availability_zone = "eu-west-1a"
 
-#   tags = {
-#     Name = "tf-project"
-#   }
-# }
-
-
-# # Then Create a network interface
-# resource "aws_network_interface" "terraform_ec2_ni" {
-#   subnet_id   = aws_subnet.terraform_ec2_subnet.id
-#   tags = {
-#     Name = "primary_network_interface"
-#   }
-# }
+  tags = {
+    Name = "tf-project"
+  }
+}
 
 
-# # Then create a security group
-# resource "aws_security_group" "terraform_ec2_sg" {
-#   name        = "terraform_ec2_sg"
-#   description = "terraform project sg for ec2 instance"
-#   vpc_id      = aws_vpc.terraform_ec2_vpc.id
+# Then Create a network interface
+resource "aws_network_interface" "terraform_ec2_ni" {
+  subnet_id   = aws_subnet.terraform_ec2_subnet.id
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
 
-#   ingress {
-#       description      = "TLS from VPC"
-#       from_port        = 22
-#       to_port          = 22
-#       protocol         = "tcp"
-#       cidr_blocks      = [aws_vpc.terraform_ec2_vpc.cidr_block]
-#     }
+
+# Then create a security group
+resource "aws_security_group" "terraform_ec2_sg" {
+  name        = "terraform_ec2_sg"
+  description = "terraform project sg for ec2 instance"
+  vpc_id      = aws_vpc.terraform_ec2_vpc.id
+
+  ingress {
+      description      = "TLS from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = [aws_vpc.terraform_ec2_vpc.cidr_block]
+    }
   
 
-#   egress {
-#       from_port        = 22
-#       to_port          = 22
-#       protocol         = "tcp"
-#       cidr_blocks      = ["0.0.0.0/0"]
-#     }
+  egress {
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
   
-# }
+}
 
 
-# # ami-0a8e758f5e873d1c1
+# ami-0a8e758f5e873d1c1
 
-# # Creating the aws resource
-# variable "amiid" {
-#     default = "ami-0a8e758f5e873d1c1"
-# }
+# Creating the aws resource
+variable "amiid" {
+    default = "ami-0a8e758f5e873d1c1"
+}
 
-# resource "aws_instance" "terraform_ec2_instance" {
-#     ami = var.amiid
-#     instance_type = "t3.micro"
-#     key_name = "terraform_project"
+resource "aws_instance" "terraform_ec2_instance" {
+    ami = var.amiid
+    instance_type = "t3.micro"
+    key_name = "terraform_project"
 
-#     network_interface {
-#         network_interface_id = aws_network_interface.terraform_ec2_ni.id
-#         device_index         = 0
-#     }
+    network_interface {
+        network_interface_id = aws_network_interface.terraform_ec2_ni.id
+        device_index         = 0
+    }
 
-#     tags = {
-#         Name = "Terraform Ec2 Instance"
-#     }
-# }
+    tags = {
+        Name = "Terraform Ec2 Instance"
+    }
+}
 
 
 
